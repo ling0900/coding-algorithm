33f7b4fdbdad825cc87a9669693c71eab6240abd
lh <992610900@qq.com>
2023-04-13 23:50:11 +0800

    题目序号：35

---
+++ coding-algorithm-leetcode/src/main/java/code/lh/algorithm/leetcode/day02/Solution.java
@@ -0,0 +1,77 @@
+package code.lh.algorithm.leetcode.day02;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The type Solution.
+ *
+ */
+public class Solution {
+    /**
+     * Exist boolean.
+     *
+     * @param board the board
+     * @param word  the word
+     * @return the boolean
+     */
+    public static boolean exist(char[][] board, String word) {
+
+        // 定义这个标记
+        boolean[][] walk = new boolean[board.length][board[0].length];
+
+        // 定义方向
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backExist(walk, i, j, board, word, 0, directions)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private static boolean backExist(boolean[][] walk, int m, int n, char[][] board, String word, int searchIndex, int[][] directions) {
+
+        if (board[m][n] != word.charAt(searchIndex)) {
+            return false;
+        } else {
+            walk[m][n] = true;
+        }
+
+        // 这里已经表明 字符 相等了，如果长度满足，则说明遍历找到了，结束。
+        if (searchIndex == word.length() - 1) {
+            return true;
+        }
+
+        boolean resultFlag = false;
+
+        for (int[] direction : directions) {// 搜索四个方向都搞一遍
+            int iNew = direction[0] + m;
+            int jNew = direction[1] + n;
+            if (iNew >= 0 && iNew < board.length && jNew >= 0 && jNew < board[0].length) {// 首先保证不越界
+                if (! walk[iNew][jNew] && backExist(walk, iNew, jNew, board, word, searchIndex + 1, directions)) {
+                    resultFlag = true;
+                    break;
+                }
+            }
+        }
+
+        // 如果所有方向遍历后依旧没有答案，说明上个为止是有问题的，需要复位，然后让外部的for去调用其他的。
+        walk[m][n] = false;
+
+        return resultFlag;
+    }
+
+    public static void main(String[] args) {
+        char[][] test = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "SEE";
+
+        boolean exist = exist(test, word);
+        System.out.println(exist);
+    }
+}
---
+++ coding-algorithm-leetcode/src/main/java/code/lh/algorithm/leetcode/day03/Solution.java
@@ -0,0 +1,100 @@
+package code.lh.algorithm.leetcode.day03;
+
+class Solution {
+    public static int[] searchRange(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+
+        // 先定义两个为止
+        int m = searchLeftIndex(nums, target);
+        int n = searchRightIndex(nums, target);
+        
+
+        int[] res = new int[0];
+        
+        if (m == -1 || n == -1) {
+            return new int[]{-1, -1};
+        }
+        
+        if (nums[m] == nums[n]) {
+            res = new int[]{m, n};
+        }
+        return res;
+    }
+
+    private static int searchLeftIndex(int [] nums, int target) {
+
+        int indexLeft = 0;
+        int indexRight = nums.length - 1;
+        int middleIndex = 0;
+        int resultIndex = 0;
+
+        while (indexLeft <= indexRight) {
+            middleIndex = (indexLeft + indexRight) / 2;
+
+            if (indexLeft == indexRight) {
+                if (nums[middleIndex] == target) {
+                    resultIndex = middleIndex;
+                }
+                break;
+            }
+
+            if (nums[middleIndex] == target) { // 那么这个为止以左有可能还有和 target 相等的
+                indexRight = middleIndex;
+                resultIndex = middleIndex;
+            } else if (nums[middleIndex] > target) { // 也是需要往左拓展区域
+                indexRight = middleIndex;
+            } else if (nums[middleIndex] < target) {
+
+                indexLeft = middleIndex + 1;
+            }
+        }
+
+        return resultIndex;
+    }
+
+    private static int searchRightIndex(int [] nums, int target) {
+
+        int indexLeft = 0;
+        int indexRight = nums.length - 1;
+        int middleIndex = 0;
+        int resultIndex = -1;
+
+        while (indexLeft <= indexRight) {
+            middleIndex = (indexLeft + indexRight) / 2;
+
+            if (indexLeft == indexRight) {
+                if (nums[middleIndex] == target) {
+                    resultIndex = middleIndex;
+                }
+                break;
+            }
+
+            if (nums[middleIndex] == target) { // 那么这个为止以左有可能还有和 target 相等的
+                resultIndex = middleIndex;
+                indexLeft = middleIndex + 1;
+            } else if (nums[middleIndex] > target) { // 也是需要往左拓展区域
+                indexRight = middleIndex - 1;
+            } else if (nums[middleIndex] < target) {
+
+                indexLeft = middleIndex + 1;
+            }
+        }
+
+        return resultIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,7,8,8,10};
+        int target = 6;
+        int i1 = searchRightIndex(nums, target);
+//        System.out.println(i1);
+        int[] i = searchRange(nums, target);
+        for (int m : i) {
+            System.out.println(m);
+        }
+    }
+}
