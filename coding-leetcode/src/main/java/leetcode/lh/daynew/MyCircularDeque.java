264f320e03845fea7f6bb2dd2c33ad79341d7e98
lh <992610900@qq.com>
2024-08-10 14:10:12 +0800

    看一下ThreadLocal的源码

---
+++ coding-leetcode/src/main/java/leetcode/lh/daynew/MyCircularDeque.java
@@ -0,0 +1,14 @@
+package leetcode.lh.daynew;
+
+/**
+ * 双端队列
+ */
+public class MyCircularDeque {
+    public static void main(String[] args) {
+        ThreadLocal<Integer> t = new ThreadLocal<>();
+        ThreadLocal<Integer> t2 = new ThreadLocal<>();
+        Integer i = t.get();
+
+    }
+
+}
