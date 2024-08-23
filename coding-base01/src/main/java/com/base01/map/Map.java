4cee80cd77f46865f9f11b0861fd07a7b474d07c
lh <992610900@qq.com>
2024-08-24 03:15:46 +0800

    ConcurrentHashMap 源码看下

---
+++ coding-base01/src/main/java/com/base01/map/Map.java
@@ -0,0 +1,16 @@
+package com.base01.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Map {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>(64, 0.75f, 4);
+        map.put("", "");
+
+
+        final int HASH_BITS = 0x7fffffff;
+
+        int h = 213132;
+        System.out.println((h ^ (h >>> 16)) & HASH_BITS);
+    }
+}
