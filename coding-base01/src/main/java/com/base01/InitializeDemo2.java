79bc5d5364b5ac3df7fb65ec99eb49954a851d07
lh <992610900@qq.com>
2024-09-03 15:07:47 +0800

    对象的实例化：构造器总是最后执行的。

---
+++ coding-base01/src/main/java/com/base01/InitializeDemo2.java
@@ -0,0 +1,45 @@
+package com.base01;
+
+public class InitializeDemo2 {
+    private static int k = 1;
+    private static int n = 10;
+    private static int i;
+    private String s = new String("-----");
+
+    static {
+        String s = "212";
+        System.out.println("静态代码块：执行了");
+        System.out.println("k=" + k + ", n=" + n + ", i=" + i);
+    }
+
+    private int h = 2;
+
+    {
+        String m = "000ppppppp";
+        System.out.println("普通代码块：执行了");
+        System.out.println(s);
+    }
+
+    public static void m1() {
+        System.out.println("静态方法：执行了");
+        System.out.println("k=" + (k++) + ", n=" + n + ", i=" + i);
+    }
+
+    public void m2() {
+        System.out.println("普通方法：执行了");
+        System.out.println("k=" + (k++) + ", n=" + n + ", i=" + i);
+    }
+
+
+    public InitializeDemo2(String str) {
+        System.out.println("构造器：执行了");
+        System.out.println("k=" + (k++) + ", n=" + n + ", i=" + i);
+    }
+
+    public static void main(String[] args) {
+        InitializeDemo2 initializeDemo2 = new InitializeDemo2("");
+        initializeDemo2.m2();
+        m1();
+
+    }
+}
