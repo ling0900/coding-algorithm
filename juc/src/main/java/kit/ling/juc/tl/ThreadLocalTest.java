9c6c0c26cc68d64769c9bc63c1a4e62c182b91a9
lh <992610900@qq.com>
2024-09-14 13:02:27 +0800

    ThreadLocalTest

--- juc/src/main/java/kit/ling/juc/tl/ThreadLocalTest.java
+++ juc/src/main/java/kit/ling/juc/tl/ThreadLocalTest.java
@@ -1,11 +1,35 @@
 package kit.ling.juc.tl;
 
+/**
+ * @author ling
+ * @date 2022/10/26 11:15
+ * @description ThreadLocalTest
+ * @since 1.0.0
+ */
 public class ThreadLocalTest {
+
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
     public static void main(String[] args) {
-        ThreadLocal threadLocal = new ThreadLocal<>();
 
-        threadLocal.set(Integer.valueOf(1));
+        // 主线程
+        threadLocal.set("0");
+        System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
+        threadLocal.remove();
 
-        threadLocal.get();
+        // 线程1
+        new Thread(() -> {
+            System.out.println("尝试获取主线程的值：" + threadLocal.get());
+            threadLocal.set("1");
+            System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
+            threadLocal.remove();
+        }, "t1").start();
+
+        new Thread(() -> {
+            threadLocal.set("2");
+            System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
+            threadLocal.remove();
+        }, "t2").start();
+
     }
 }
